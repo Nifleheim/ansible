a3e21318c35a0676b0bfe4e95ffec7dfaaa2b5af
Nifleheim <reza.fabryan@gmail.com>
2023-06-06 01:04:51 +0700

    add delete sh file

---
+++ delete.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo rm -rf ansible-back
+sudo rm -rf ansible-back@tmp
+sudo rm -rf ansible-deploy
+sudo rm -rf ansible-deploy@tmp
+sudo rm -rf ansible-efk
+sudo rm -rf ansible-efk@tmp
+sudo rm -rf ansible-kibana
+sudo rm -rf ansible-kibana@tmp
+sudo rm -rf ansible-stop@tmp
